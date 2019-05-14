d77723e2ed6e534092f5a5cd3079e3ae1ff191e2
Minh Pham <minhpham@Amin.local>
2019-05-14 21:38:25 +0700

    Add router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Note from "./../Note";
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <Note />
-      </div>
+      <Router>
+        <Route path="/" exact component={Note} />
+      </Router>
     );
   }
 }
