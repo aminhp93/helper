148a68e023b9f42160839fe16454207fe0c5f84b
Minh Pham <minhpham@Amin.local>
2019-06-15 19:53:35 +0700

    Build

--- src/components/CheckList/CheckList.js
+++ src/components/CheckList/CheckList.js
@@ -18,158 +18,155 @@ class CheckList extends Component {
   constructor(props) {
     super(props);
     const that = this;
-    this.rowData = [];
-    this.state = {
-      columnDefs: [
-        {
-          field: "id",
-          rowDrag: true,
-          cellRenderer: function(params) {
-            const div = document.createElement("div");
-            div.className = "actionButtons";
-            const deleteRowButton = document.createElement("div");
-            const progressLinear = document.createElement("div");
-            deleteRowButton.className = "deleteRowButton";
-            ReactDOM.render(
-              <div onClick={() => that.handleDelete(params.data.id, params)}>
-                <Icon>delete</Icon>
-              </div>,
-              deleteRowButton
-            );
-            // ReactDOM.render(
-            //   <div>{params.data.is_doing ? <LinearProgress /> : null}</div>, progressLinear
-            // )
-            progressLinear.className = "progressLinear";
-            div.innerText = params.data.id;
-            div.appendChild(progressLinear);
-            div.appendChild(deleteRowButton);
 
-            return div;
-          }
-        },
-        {
-          field: "Done",
-          width: 50,
-          cellRenderer: function(params) {
-            const div = document.createElement("div");
-            ReactDOM.render(
-              <CustomedCheckBox
-                checked={params.data.is_done}
-                cb={data =>
-                  that.handleCbCheckBox(data, params.data.id, "is_done")
-                }
-              />,
-              div
-            );
-            return div;
-          }
-        },
-        {
-          field: "content",
-          width: 500,
-          cellRenderer: function(params) {
-            const div = document.createElement("div");
-            ReactDOM.render(
-              <CustomedInput
-                value={params.data.content}
-                cb={content => that.handleCbInput({ content }, params.data.id)}
-              />,
-              div
-            );
-            return div;
-          }
-        },
-        {
-          field: "In Progress",
-          width: 70,
-          cellRenderer: function(params) {
-            console.log(84);
-            const div = document.createElement("div");
-            ReactDOM.render(
-              <div>
-                <CustomedCheckBox
-                  checked={params.data.is_doing}
-                  cb={data =>
-                    that.handleCbCheckBox(data, params.data.id, "is_doing")
-                  }
-                />
-              </div>,
-              div
-            );
-            return div;
-          }
-        },
-        {
-          headerName: "Repeated card",
-          field: "",
-          cellRenderer: function(params) {
-            const div = document.createElement("div");
-            ReactDOM.render(<RepeatedCard />, div);
-            return div;
-          }
-        },
-        {
-          field: "default_cost",
-          width: 100,
-          cellRenderer: function(params) {
-            const div = document.createElement("div");
-            ReactDOM.render(
-              <CustomedInput
-                type="float"
-                value={params.data.default_cost}
-                cb={default_cost =>
-                  that.handleCbInput({ default_cost }, params.data.id)
-                }
-              />,
-              div
-            );
-            return div;
-          }
-        },
-        {
-          field: "actual_cost",
-          width: 100,
-          cellRenderer: function(params) {
-            const div = document.createElement("div");
-            ReactDOM.render(
-              <CustomedInput
-                type="float"
-                value={params.data.actual_cost}
-                cb={actual_cost =>
-                  that.handleCbInput({ actual_cost }, params.data.id)
-                }
-              />,
-              div
-            );
-            return div;
-          }
-        },
-        {
-          field: "scheduled_time",
-          cellRenderer: function(params) {
-            const div = document.createElement("div");
-            ReactDOM.render(
-              <CustomedInput
-                type="float"
-                value={params.data.scheduled_time}
-                cb={scheduled_time =>
-                  that.handleCbInput({ scheduled_time }, params.data.id)
-                }
-              />,
-              div
-            );
-            return div;
-          }
+    this.columnDefs = [
+      {
+        field: "id",
+        rowDrag: true,
+        cellRenderer: function(params) {
+          const div = document.createElement("div");
+          div.className = "actionButtons";
+          const deleteRowButton = document.createElement("div");
+          const progressLinear = document.createElement("div");
+          deleteRowButton.className = "deleteRowButton";
+          ReactDOM.render(
+            <div onClick={() => that.handleDelete(params.data.id, params)}>
+              <Icon>delete</Icon>
+            </div>,
+            deleteRowButton
+          );
+          // ReactDOM.render(
+          //   <div>{params.data.is_doing ? <LinearProgress /> : null}</div>, progressLinear
+          // )
+          progressLinear.className = "progressLinear";
+          div.innerText = params.data.id;
+          div.appendChild(progressLinear);
+          div.appendChild(deleteRowButton);
+
+          return div;
         }
-      ],
-      defaultColDef: {
-        width: 150,
-        sortable: true,
-        filter: true,
-        resizable: true,
-        enableCellChangeFlash: true
       },
-      rowData: []
+      {
+        field: "Done",
+        width: 50,
+        cellRenderer: function(params) {
+          const div = document.createElement("div");
+          ReactDOM.render(
+            <CustomedCheckBox
+              checked={params.data.is_done}
+              cb={data =>
+                that.handleCbCheckBox(data, params.data.id, "is_done")
+              }
+            />,
+            div
+          );
+          return div;
+        }
+      },
+      {
+        field: "content",
+        width: 500,
+        cellRenderer: function(params) {
+          const div = document.createElement("div");
+          ReactDOM.render(
+            <CustomedInput
+              value={params.data.content}
+              cb={content => that.handleCbInput({ content }, params.data.id)}
+            />,
+            div
+          );
+          return div;
+        }
+      },
+      {
+        field: "In Progress",
+        width: 70,
+        cellRenderer: function(params) {
+          const div = document.createElement("div");
+          ReactDOM.render(
+            <div>
+              <CustomedCheckBox
+                checked={params.data.is_doing}
+                cb={data =>
+                  that.handleCbCheckBox(data, params.data.id, "is_doing")
+                }
+              />
+            </div>,
+            div
+          );
+          return div;
+        }
+      },
+      {
+        headerName: "Repeated card",
+        field: "",
+        cellRenderer: function(params) {
+          const div = document.createElement("div");
+          ReactDOM.render(<RepeatedCard />, div);
+          return div;
+        }
+      },
+      {
+        field: "default_cost",
+        width: 100,
+        cellRenderer: function(params) {
+          const div = document.createElement("div");
+          ReactDOM.render(
+            <CustomedInput
+              type="float"
+              value={params.data.default_cost}
+              cb={default_cost =>
+                that.handleCbInput({ default_cost }, params.data.id)
+              }
+            />,
+            div
+          );
+          return div;
+        }
+      },
+      {
+        field: "actual_cost",
+        width: 100,
+        cellRenderer: function(params) {
+          const div = document.createElement("div");
+          ReactDOM.render(
+            <CustomedInput
+              type="float"
+              value={params.data.actual_cost}
+              cb={actual_cost =>
+                that.handleCbInput({ actual_cost }, params.data.id)
+              }
+            />,
+            div
+          );
+          return div;
+        }
+      },
+      {
+        field: "scheduled_time",
+        cellRenderer: function(params) {
+          const div = document.createElement("div");
+          ReactDOM.render(
+            <CustomedInput
+              type="float"
+              value={params.data.scheduled_time}
+              cb={scheduled_time =>
+                that.handleCbInput({ scheduled_time }, params.data.id)
+              }
+            />,
+            div
+          );
+          return div;
+        }
+      }
+    ];
+
+    this.defaultColDef = {
+      width: 150,
+      sortable: true,
+      filter: true,
+      resizable: true,
+      enableCellChangeFlash: true
     };
   }
 
@@ -215,6 +212,7 @@ class CheckList extends Component {
   }
 
   handleCbCheckBox(checked_value, id, index) {
+    console.log();
     const url = getUpdateNoteUrl(id);
     let done_time = new Date().getTime();
     let data = { id };
@@ -226,30 +224,28 @@ class CheckList extends Component {
     } else if (index === "is_doing") {
       data.is_doing = checked_value;
     }
+    // this.gridApi.forEachNode((node, indexNode) => {
+    //   console.log(node.data);
+    //   if (node.data.is_doing) {
+    //     node.data.is_doing = false;
+    //   }
+    //   if (node.data.id === id) {
+    //     node.data.is_doing = checked_value;
+    //     node.setData(node.data);
+    //     this.gridApi.refreshCells();
+
+    //   }
+    //   console.log(node.data);
+    // });
+    // this.forceUpdate();
+
     axios
       .post(url, data)
       .then(response => {
         console.log(response);
-        // const url1 = getAllNotesUrl();
-        // axios
-        //   .get(url1)
-        //   .then(response => {
-        //     if (
-        //       response.data &&
-        //       response.data.posts &&
-        //       response.data.posts.length
-        //     ) {
-        //       // this.gridApi.setRowData(response.data.posts);
-        //       // this.gridApi.sizeColumnsToFit();
-
-        //       this.setState({
-        //         rowData: response.data.posts
-        //       })
-        //     }
-        //   })
-        //   .catch(error => {
-        //     console.log(error);
-        //   });
+        if (index === "is_doing") {
+          this.getAllNotes();
+        }
       })
       .catch(error => {
         console.log(error);
@@ -266,12 +262,8 @@ class CheckList extends Component {
           response.data.posts &&
           response.data.posts.length
         ) {
-          // this.gridApi.setRowData(response.data.posts);
-          // this.gridApi.sizeColumnsToFit();
-          this.rowData = response.data.posts;
-          this.setState({
-            rowData: response.data.posts
-          });
+          this.gridApi.setRowData(response.data.posts);
+          this.gridApi.sizeColumnsToFit();
         }
       })
       .catch(error => {
@@ -317,15 +309,6 @@ class CheckList extends Component {
   onRowDragEnd(e) {
     let obj = this.gridApi.getRowNode(e.overIndex).data;
     console.log(e, obj, obj.scheduled_time);
-    // e.node.data.scheduled_time =
-    //   this.gridApi.getRowNode(e.overIndex).data.scheduled_time + 0.01;
-    // this.gridApi.updateRowData({
-    //   update: [e.node.data]
-    // });
-    // e.node.setDataValue(
-    //   "scheduled_time",
-    //   this.gridApi.getRowNode(e.overIndex).data.scheduled_time + 0.01
-    // );
   }
 
   render() {
@@ -336,12 +319,11 @@ class CheckList extends Component {
         <div />
         <div className="ag-theme-balham agGridContainer">
           <AgGridReact
-            columnDefs={this.state.columnDefs}
-            defaultColDef={this.state.defaultColDef}
+            columnDefs={this.columnDefs}
+            defaultColDef={this.defaultColDef}
             rowDragManaged={true}
             animateRows={true}
             onGridReady={this.onGridReady}
-            rowData={this.state.rowData}
             onRowDragEnd={this.onRowDragEnd.bind(this)}
             onRowDragStart={this.onRowDragStart.bind(this)}
             // headerHeight={0}
--- src/components/_customedComponents/CustomedCheckBox/CustomedCheckBox.js
+++ src/components/_customedComponents/CustomedCheckBox/CustomedCheckBox.js
@@ -1,26 +1,37 @@
-import Checkbox from '@material-ui/core/Checkbox';
-import React from 'react';
+import Checkbox from "@material-ui/core/Checkbox";
+import React from "react";
 
 class CustomedCheckBox extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            checked: props.checked
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      checked: props.checked
+    };
+  }
 
-    handleChange() {
-        this.setState({
-            checked: !this.state.checked
-        }, () => {
-            this.props.cb(this.state.checked)
-        })
+  componentWillReceiveProps(nextProps) {
+    console.log(nextProps);
+  }
 
-    }
+  handleChange() {
+    this.setState(
+      {
+        checked: !this.state.checked
+      },
+      () => {
+        this.props.cb(this.state.checked);
+      }
+    );
+  }
 
-    render() {
-        return <Checkbox checked={this.state.checked} onChange={this.handleChange.bind(this)} />
-    }
+  render() {
+    return (
+      <Checkbox
+        checked={this.state.checked}
+        onChange={this.handleChange.bind(this)}
+      />
+    );
+  }
 }
 
-export default CustomedCheckBox
+export default CustomedCheckBox;
