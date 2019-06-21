f29aafe31490da2aac98cf52dbcf66918dbd610b
Minh Pham <minhpham@bocasay.com>
2019-06-21 14:05:29 +0700

    Deploy

--- src/components/Stock/Stock.js
+++ src/components/Stock/Stock.js
@@ -59,7 +59,7 @@ class Stock extends Component {
       {
         headerName: "Symbol",
         field: "Symbol",
-        cellRenderer: function(params) {
+        cellRenderer: function (params) {
           const div = document.createElement("div");
           div.className = "symbolCellContainer";
           const container = document.createElement("div");
@@ -72,12 +72,12 @@ class Stock extends Component {
           content.className = "content";
           detail.className = "detail";
           ReactDOM.render(<Icon>info</Icon>, detail);
-          detail.addEventListener("click", function() {
+          detail.addEventListener("click", function () {
             that.openModal(params);
           });
           ReactDOM.render(<Icon>delete</Icon>, deleteButton);
           deleteButton.className = "deleteButton";
-          deleteButton.addEventListener("click", function() {
+          deleteButton.addEventListener("click", function () {
             that.deleteSymbolWatchlist(params);
           });
 
@@ -93,7 +93,7 @@ class Stock extends Component {
         headerName: "TodayCapitalization",
         field: "today_capitalization",
         filter: "agNumberColumnFilter",
-        cellRenderer: function(params) {
+        cellRenderer: function (params) {
           return (params.data.today_capitalization / Math.pow(10, 9)).toFixed(
             0
           );
@@ -103,7 +103,7 @@ class Stock extends Component {
         headerName: "% Change in Price",
         field: "percentage_change_in_price",
         filter: "agNumberColumnFilter",
-        cellRenderer: function(params) {
+        cellRenderer: function (params) {
           if (params.data.percentage_change_in_price) {
             return (params.data.percentage_change_in_price * 100).toFixed(2);
           }
@@ -113,7 +113,7 @@ class Stock extends Component {
         headerName: "Close",
         field: "Close",
         filter: "agNumberColumnFilter",
-        cellRenderer: function(params) {
+        cellRenderer: function (params) {
           if (params.data.Close) {
             return params.data.Close.toFixed(0);
           }
@@ -123,7 +123,7 @@ class Stock extends Component {
         headerName: "% Change in Volume",
         field: "percentage_change_in_volume",
         filter: "agNumberColumnFilter",
-        cellRenderer: function(params) {
+        cellRenderer: function (params) {
           if (params.data.percentage_change_in_volume) {
             return (params.data.percentage_change_in_volume * 100).toFixed(2);
           }
@@ -138,7 +138,7 @@ class Stock extends Component {
         headerName: "ROE",
         field: "ROE",
         filter: "agNumberColumnFilter",
-        cellRenderer: function(params) {
+        cellRenderer: function (params) {
           if (params.data.ROE) {
             return params.data.ROE.toFixed(0);
           }
@@ -148,7 +148,7 @@ class Stock extends Component {
         headerName: "EPS",
         field: "EPS",
         filter: "agNumberColumnFilter",
-        cellRenderer: function(params) {
+        cellRenderer: function (params) {
           if (params.data.EPS) {
             return params.data.EPS.toFixed(0);
           }
@@ -168,7 +168,7 @@ class Stock extends Component {
         headerName: "MarketCapitalization",
         field: "MarketCapitalization",
         filter: "agNumberColumnFilter",
-        cellRenderer: function(params) {
+        cellRenderer: function (params) {
           if (params.data.MarketCapitalization) {
             return params.data.MarketCapitalization.toFixed(0);
           }
@@ -199,16 +199,17 @@ class Stock extends Component {
   startRealtimeSocket(dataStocks) {
     const that = this;
     const socket = new WebSocket(
-      "wss://www.fireant.vn/signalr/connect?transport=webSockets&clientProtocol=1.5&SessionID=ubjd4qzzvyjzmiisz0infqw3&connectionToken=65Io4MIjtEg35eA6eCpaoEuVEa%2Bq0dXWmCKk9iXItWBq5wv4%2Bx3nN87hxatafb2iwwRe9YEl5LeWdZQsqulAhWC%2FDtl%2FkVIcVB4FEynbjpTtMxsH%2BOkMOpSyrAdbOjjNMoeB%2BQ%3D%3D&connectionData=%5B%7B%22name%22%3A%22compressedappquotehub%22%7D%5D&tid=1"
+      // "wss://www.fireant.vn/signalr/connect?transport=webSockets&clientProtocol=1.5&SessionID=ubjd4qzzvyjzmiisz0infqw3&connectionToken=65Io4MIjtEg35eA6eCpaoEuVEa%2Bq0dXWmCKk9iXItWBq5wv4%2Bx3nN87hxatafb2iwwRe9YEl5LeWdZQsqulAhWC%2FDtl%2FkVIcVB4FEynbjpTtMxsH%2BOkMOpSyrAdbOjjNMoeB%2BQ%3D%3D&connectionData=%5B%7B%22name%22%3A%22compressedappquotehub%22%7D%5D&tid=1"
+      "wss://svr2.fireant.vn/signalr/connect?transport=webSockets&clientProtocol=1.5&SessionID=ubjd4qzzvyjzmiisz0infqw3&connectionToken=Dxy2ne8X%2F3wFZ4D9Cbrwo7dA9M9asvooPV4Gz4jR6kGsdt%2BIKWmtRgjNMwSHKi7DCyczB%2FQzBRWScoSuRGkHQQnOG7O%2Bi6k89%2BPBorAaCuknM892dJzec9CqzaySCIvZ7zAwyg%3D%3D&connectionData=%5B%7B%22name%22%3A%22compressedappquotehub%22%7D%5D&tid=3"
     );
 
     // Connection opened
-    socket.addEventListener("open", function(event) {
+    socket.addEventListener("open", function (event) {
       socket.send("Hello Server!");
     });
 
     // Listen for messages
-    socket.addEventListener("message", function(event) {
+    socket.addEventListener("message", function (event) {
       // console.log(event.data);
 
       let data = event.data;
@@ -258,7 +259,7 @@ class Stock extends Component {
                   } else {
                     let new_stock = response.data.stock;
                     // console.log(new_stock, that.gridApi, index)
-                    that.gridApi.forEachNode(function(node) {
+                    that.gridApi.forEachNode(function (node) {
                       if (node.data.id === new_stock.id) {
                         // console.log(node.data);
                         node.setData({ ...node.data, new_stock });
@@ -399,7 +400,7 @@ class Stock extends Component {
             headerName: `Test${i}`,
             field: "",
             width: 70,
-            cellRenderer: function(params) {
+            cellRenderer: function (params) {
               console.log(params);
               return params.data[i];
             }
