478faff4650df2d55a9590384df03e569528f74a
Minh Pham <minhpham@Amin-2.local>
2019-06-23 20:56:08 +0700

    Step 3: Setup a basic Node server

--- server.js
+++ server.js
@@ -1,12 +1,41 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import Chatkit from "@pusher/chatkit-server";
 
 const app = express();
+
+const chatkit = new Chatkit.default({
+  instanceLocator: "v1:us1:c89d2d68-a0df-4ed6-82b0-ac8ea2d0d7f1",
+  key:
+    "823d9322-5290-4b11-acb0-6e44e4fcca21:/zU1bBU1/y5OASq6QnqHfOpQXM0WBu88qwL3tXCPUwg="
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+
+app.post("/users", (req, res) => {
+  const { username } = res.body;
+  chatkit
+    .createUser({
+      id: username,
+      name: username
+    })
+    .then(() => res.sendStatus(201))
+    .cathc(error => {
+      if (error.error === "services/chatkit/user_already_exists") {
+        res.sendStatus(200);
+      } else {
+        res.status(error.status).json(error);
+      }
+    });
+});
+
+app.post("/authenticate", (req, res) => {
+  const authData = chatkit.authenticate({ userId: req.query.user_id });
+  res.status(authData.status).send(authData.body);
+});
 
 const PORT = 3001;
 app.listen(PORT, err => {
