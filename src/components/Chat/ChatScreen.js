47dc070ad087d60852b97b69925e556cf51cc4a8
Minh Pham <minhpham@Amin-2.local>
2019-06-23 21:35:10 +0700

    Create basic UI layout

--- src/components/Chat/ChatScreen.js
+++ src/components/Chat/ChatScreen.js
@@ -1,10 +1,67 @@
 import React from "react";
+import Chatkit from "@pusher/chatkit-client";
 
 class ChatScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: {}
+    };
+  }
+
+  componentDidMount() {
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator: "v1:us1:c89d2d68-a0df-4ed6-82b0-ac8ea2d0d7f1",
+      userId: this.props.currentUsername,
+      tokenProvider: new Chatkit.TokenProvider({
+        url: "http://localhost:3001/authenticate"
+      })
+    });
+
+    chatManager
+      .connect()
+      .then(currentUser => {
+        this.setState({ currentUser });
+      })
+      .catch(error => console.log("error", error));
+  }
+
   render() {
+    const styles = {
+      container: {
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column"
+      },
+      chatContainer: {
+        display: "flex",
+        flex: 1
+      },
+      whosOnlineListContainer: {
+        width: "300px",
+        flex: "none",
+        padding: 20,
+        backgroundColor: "#2c303b",
+        color: "white"
+      },
+      chatListContainer: {
+        padding: 20,
+        width: "85%",
+        display: "flex",
+        flexDirection: "column"
+      }
+    };
+
     return (
-      <div>
-        <h1>Chat</h1>
+      <div style={styles.container}>
+        <div style={styles.chatContainer}>
+          <aside style={styles.whosOnlineListContainer}>
+            <h2>Who's online PLACEHOLDER</h2>
+          </aside>
+          <section style={styles.chatListContainer}>
+            <h2>Chat PLACEHOLDER</h2>
+          </section>
+        </div>
       </div>
     );
   }
