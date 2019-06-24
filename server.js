e2485c2d3284cb72c376b9aa19a7076ee67fb3db
Minh Pham <minhpham@bocasay.com>
2019-06-24 14:09:33 +0700

    Build

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const Chatkit = require("@pusher/chatkit-server");
 const app = express();
 
 const chatkit = new Chatkit.default({
-  instanceLocator: "v1:us1:c89d2d68-a0df-4ed6-82b0-ac8ea2d0d7f1",
-  key:
-    "823d9322-5290-4b11-acb0-6e44e4fcca21:/zU1bBU1/y5OASq6QnqHfOpQXM0WBu88qwL3tXCPUwg="
+    instanceLocator: "v1:us1:c89d2d68-a0df-4ed6-82b0-ac8ea2d0d7f1",
+    key:
+        "823d9322-5290-4b11-acb0-6e44e4fcca21:/zU1bBU1/y5OASq6QnqHfOpQXM0WBu88qwL3tXCPUwg="
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,39 +16,39 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  return res.json({ test: "success" });
+    return res.json({ test: "success" });
 });
 
 app.post("/users", (req, res) => {
-  const { username } = req.body;
-  chatkit
-    .createUser({
-      id: username,
-      name: username
-    })
-    .then(res1 => {
-      res.sendStatus(201);
-    })
-    .catch(error => {
-      console.log(error);
-      if (error.error === "services/chatkit/user_already_exists") {
-        res.sendStatus(200);
-      } else {
-        res.status(error.status).json(error);
-      }
-    });
+    const { username } = req.body;
+    chatkit
+        .createUser({
+            id: username,
+            name: username
+        })
+        .then(res1 => {
+            res.sendStatus(201);
+        })
+        .catch(error => {
+            console.log(error);
+            if (error.error === "services/chatkit/user_already_exists") {
+                res.sendStatus(200);
+            } else {
+                res.status(error.status).json(error);
+            }
+        });
 });
 
 app.post("/authenticate", (req, res) => {
-  const authData = chatkit.authenticate({ userId: req.query.user_id });
-  res.status(authData.status).send(authData.body);
+    const authData = chatkit.authenticate({ userId: req.query.user_id });
+    res.status(authData.status).send(authData.body);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, err => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(`Running on port ${PORT}`);
-  }
+    if (err) {
+        console.error(err);
+    } else {
+        console.log(`Running on port ${PORT}`);
+    }
 });
