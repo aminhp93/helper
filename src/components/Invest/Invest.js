a2d13055935fdca1f704973d2178e98102da2abe
MINH PHAM <minhpn.org.ec@gmail.com>
2019-08-04 17:28:02 +0700

    Add calculate stragety

--- src/components/Invest/Invest.js
+++ src/components/Invest/Invest.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Tabs, Input, Button, Row, Col } from "antd";
+import { Tabs, Input, Button, Row, Col, Table, Divider, Tag } from "antd";
 
 const InputGroup = Input.Group;
 
@@ -9,7 +9,82 @@ const tienChoThueNha_default = 17;
 const tienLuong_default = 37;
 const laiSuatNganHang_default = 0.07;
 const noHientai_default = 400;
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: text => <a href="javascript:;">{text}</a>,
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    render: tags => (
+      <span>
+        {tags.map(tag => {
+          let color = tag.length > 5 ? 'geekblue' : 'green';
+          if (tag === 'loser') {
+            color = 'volcano';
+          }
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </span>
+    ),
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (text, record) => (
+      <span>
+        <a href="javascript:;">Invite {record.name}</a>
+        <Divider type="vertical" />
+        <a href="javascript:;">Delete</a>
+      </span>
+    ),
+  },
+];
 
+const data = [
+  {
+    key: '1',
+    year: '2020',
+    data: [{
+      FPTS: '25',
+      timo: '45',
+      ngan_hang: '54',
+      trai_phieu: '251',
+    }]
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
 class Invest extends React.Component {
   constructor(props) {
     super(props);
@@ -132,7 +207,7 @@ class Invest extends React.Component {
             </InputGroup>
           </TabPane>
           <TabPane tab="Tab 2" key="2">
-            Content of Tab Pane 2
+            <Table columns={columns} dataSource={data} />
           </TabPane>
           <TabPane tab="Tab 3" key="3">
             Content of Tab Pane 3
