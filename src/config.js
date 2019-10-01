8c651509ba0f0a0a7bb022b87aceef36eae4ffb9
MINH PHAM <minhpn.org.ec@gmail.com>
2019-10-01 10:42:50 +0700

    Build

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-  // isProduction: true,
-  isProduction: false,
+  isProduction: true,
+  // isProduction: false,
   instanceLocator: "v1:us1:c89d2d68-a0df-4ed6-82b0-ac8ea2d0d7f1",
   key:
     "823d9322-5290-4b11-acb0-6e44e4fcca21:/zU1bBU1/y5OASq6QnqHfOpQXM0WBu88qwL3tXCPUwg="
